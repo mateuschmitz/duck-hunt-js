d02ce557fc6de51b0e322ca9c0b0176433ce3728
Mateus Schmitz <matteuschmitz@gmail.com>
2014-10-04 11:19:44 -0300

    Start project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>Caça aos patos</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body>
+		<div class="hunt-space">
+			<img src="image/duck-flying-right.png" class='duck-flying-one' onclick="alert('BAAANG');">
+			<img src="image/duck-flying-left.png" class='duck-flying-two' onclick="alert('BAAANG');">
+		</div>
+	</body>
+</html>
