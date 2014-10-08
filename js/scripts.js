6395c8fe765826fa18bf353d31fc39b930c9f58c
Mateus Schmitz <matteuschmitz@gmail.com>
2014-10-07 23:28:21 -0300

    Ducks gerados aleatoriamente e mostrados na tela

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
+/**
+ */
 window.onload = function () {
-	generateAleatoryDuck();
+	while ((document.getElementsByClassName('duck-flying').length) < 5) {
+		
+		generateAleatoryDuck();
+		setTimeout(1000);
+
+	}
 };
+
+function generateAleatoryDuck () {
+
+	var animationClass = ['one', 'two'];
+	var duck = document.createElement('img');
+
+	for (var i in animationClass) {
+		if ((document.getElementsByClassName(animationClass[i]).length) === 0) {
+			duck.setAttribute('src', 'image/duck-flying-' + animationClass[i] + '.png');
+			duck.setAttribute('class', 'duck-flying ' + animationClass[i]);
+			break;
+		}
+	}
+
+	duck.setAttribute('onclick', 'return rightShot();');
+	document.getElementById('hunt-space').appendChild(duck);
+}
 
 shot = function() {
 	numBullets = document.getElementById('num-bullets').innerHTML;
@@ -12,24 +36,11 @@ shot = function() {
 	}
 };
 
-function generateAleatoryDuck () {
-	var duck = document.createElement('img');
-	
-	duck.setAttribute('src', 'image/duck-flying-right.png');
-	duck.setAttribute('class', 'duck-flying-one');
-	duck.setAttribute('onclick', 'return rightShot();');
-
-	document.getElementById('hunt-space').appendChild(duck);
-};
-
-
-
-
-
 rightShot = function() {
 	numAssertions = document.getElementById('num-assertions').innerHTML;
 	numAssertions++;
 
+	this.style.display = 'none';
 	document.getElementById('num-assertions').innerHTML = numAssertions;
 };
 
