07223b9152269725251950a3f170c2c9bc6d3c21
Mateus Schmitz <matteuschmitz@gmail.com>
2014-10-12 15:13:40 -0300

    Add ID Analytics

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
 		<meta charset='utf-8'>
 		<title>Duck Junt Js</title>
 		<link rel="stylesheet" type="text/css" href="css/styles.min.css">
+		<script>
+  			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  			ga('create', 'UA-55666991-1', 'auto');
+  			ga('send', 'pageview');
+		</script>
 	</head>
 	<body>
 		<div class='layer'></div>
